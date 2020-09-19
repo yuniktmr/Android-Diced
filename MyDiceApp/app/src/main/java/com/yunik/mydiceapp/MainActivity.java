b28a336efa3e62943d7f52143f8d20125b17c3ac
Yunik Tamrakar <yuniktmr@gmail.com>
2020-09-18 22:54:28 -0600

    Finished Product

---
+++ MyDiceApp/app/src/main/java/com/yunik/mydiceapp/MainActivity.java
@@ -0,0 +1,64 @@
+package com.yunik.mydiceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView diceImage1 = findViewById(R.id.imgDice1);
+        final ImageView diceImage2 = findViewById(R.id.imgDice2);
+
+        final int[] diceImages = {R.drawable.dice1,R.drawable.dice2,
+                            R.drawable.dice3,R.drawable.dice4,
+                            R.drawable.dice5,R.drawable.dice6};
+
+        Button button = findViewById(R.id.btnRollTheDice);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.dice_sound);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("MyDiceApp","btn roll tapped");
+
+                Random rand = new Random();
+
+                int myRandomNumber = rand.nextInt(6);
+
+                Log.i("MyDiceApp", myRandomNumber+"");
+                System.out.println("ok");
+
+                diceImage1.setImageResource(diceImages[myRandomNumber]);
+                myRandomNumber = rand.nextInt(6);
+
+                diceImage2.setImageResource(diceImages[myRandomNumber]);
+
+                YoYo.with(Techniques.Shake)
+                        .duration(400)
+                        .repeat(0)
+                        .playOn(diceImage1);
+                YoYo.with(Techniques.Shake)
+                        .duration(400)
+                        .repeat(0)
+                        .playOn(diceImage2);
+
+                mp.start();
+            }
+        });
+    }
+}
